866bddf98ac58d4ec025795604d5e4466a0aebc8
Asmaa Badry <98457159+AsmaaBadry@users.noreply.github.com>
2024-05-12 23:57:04 +0300

    Add files via upload

---
+++ books.js
@@ -0,0 +1,74 @@
+let books = [
+  {
+    ISBN: '123456789',
+    NAME: 'Brave New World',
+    CATEGORY: 'Classic',
+    IMAGE: 'https://images.penguinrandomhouse.com/cover/9780060850524',
+    NUMBER_OF_PAGES: 40,
+    AUTHOR: 'Aldous Huxley',
+    PRICE: 15
+  },
+  {
+    ISBN: '987654321',
+    NAME: 'The Picture of Dorian Gray',
+    CATEGORY: 'Classic',
+    IMAGE: 'https://images.penguinrandomhouse.com/cover/9780141442464',
+    NUMBER_OF_PAGES: 20,
+    AUTHOR: 'Oscar Wilde',
+    PRICE: 12
+  },
+  {
+    ISBN: '456789123',
+    NAME: 'Fahrenheit 451',
+    CATEGORY: 'Dystopian',
+    IMAGE: 'https://images.penguinrandomhouse.com/cover/9781451673319',
+    NUMBER_OF_PAGES: 328,
+    AUTHOR: 'Ray Bradbury',
+    PRICE: 10
+  },{
+    ISBN: '234567890',
+    NAME: 'The Name of the Wind',
+    CATEGORY: 'Fantasy',
+    IMAGE: 'https://images.penguinrandomhouse.com/cover/9780756404741',
+    NUMBER_OF_PAGES: 48,
+    AUTHOR: 'Patrick Rothfuss',
+    PRICE: 18
+  },
+  {
+    ISBN: '345678901',
+    NAME: 'Jane Eyre',
+    CATEGORY: 'Romance',
+    IMAGE: 'https://images.penguinrandomhouse.com/cover/9780143106159',
+    NUMBER_OF_PAGES: 432,
+    AUTHOR: 'Charlotte Brontë',
+    PRICE: 14
+  },
+  {
+    ISBN: '567890123',
+    NAME: 'Moby-Dick',
+    CATEGORY: 'Classic',
+    IMAGE: 'https://images.penguinrandomhouse.com/cover/9780142437247',
+    NUMBER_OF_PAGES: 288,
+    AUTHOR: 'Herman Melville',
+    PRICE: 11
+  },
+  {
+    ISBN: '789012345',
+    NAME: 'Alice in Wonderland',
+    CATEGORY: 'Fantasy',
+    IMAGE: 'https://images.penguinrandomhouse.com/cover/9780141439761',
+    NUMBER_OF_PAGES: 35,
+    AUTHOR: 'Lewis Carroll',
+    PRICE: 16
+  },
+  {
+    ISBN: '890123456',
+    NAME: 'The Chronicles of Narnia',
+    CATEGORY: 'Fantasy',
+    IMAGE: 'https://images.penguinrandomhouse.com/cover/9780066238500',
+    NUMBER_OF_PAGES: 1178,
+    AUTHOR: 'C.S. Lewis',
+    PRICE: 25
+  },
+  
+];
